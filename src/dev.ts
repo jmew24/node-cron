ae6996832b8f4ef0f20cfe472ac3449edf6c19c4
James <james@jmew.ca>
2022-12-02 01:11:57 -0500

    Add Dev script

---
+++ src/dev.ts
@@ -0,0 +1,122 @@
+import prisma from './lib/prisma';
+import getHockey from './external/hockey';
+import getBaseball from './external/baseball';
+import getFootball from './external/football';
+import getCFL from './external/cfl';
+import getBasketball, { getWNBA } from './external/basketball';
+import getMLS from './external/mls';
+import getESPNSoccer from './external/espnSoccer';
+import getATP, { getWTA } from './external/tennis';
+
+process.on('SIGTERM', async () => {
+  try {
+    await prisma.$disconnect();
+  } catch {}
+
+  console.log('---------------------');
+  console.log(`[${new Date()}] Ended cron job scheduler...`);
+  console.log('---------------------');
+});
+
+const startUp = async () => {
+  console.log('---------------------');
+  console.log(`[${new Date()}] Starting cron job scheduler...`);
+  console.log('---------------------');
+
+  /**
+  // Get all NHL players.
+  console.log(`[${new Date()}] Running NHL Logging`);
+  await getHockey();
+  console.log(`[${new Date()}] Completed NHL Logging`);
+  /**/
+
+  /**
+  // Get all MLB players.
+  console.log(`[${new Date()}] Running MLB Logging`);
+  await getBaseball();
+  console.log(`[${new Date()}] Completed MLB Logging`);
+  /**/
+
+  /**
+  // Get all NFL players.
+  console.log(`[${new Date()}] Running NFL Logging`);
+  await getFootball();
+  console.log(`[${new Date()}] Completed NFL Logging`);
+  /**/
+
+  /**
+  // Get all CFL players.
+  console.log(`[${new Date()}] Running CFL Logging`);
+  await getCFL();
+  console.log(`[${new Date()}] Completed CFL Logging`);
+  /**/
+
+  /**
+  // Get all NBA players.
+  console.log(`[${new Date()}] Running NBA Logging`);
+  await getBasketball();
+  console.log(`[${new Date()}] Completed NBA Logging`);
+  /**/
+
+  /**
+  // Get all WNBA players.
+  console.log(`[${new Date()}] Running WNBA Logging`);
+  await getWNBA();
+  console.log(`[${new Date()}] Completed WNBA Logging`);
+  /**/
+
+  /**
+  // Get all MLS players.
+  console.log(`[${new Date()}] Running MLS Logging`);
+  await getMLS();
+  console.log(`[${new Date()}] Completed MLS Logging`);
+  /**/
+
+  /**
+  // Get all Premier League players.
+  console.log(`[${new Date()}] Running English Premier League Logging`);
+  await getESPNSoccer('eng.1', 'English Premier League');
+  console.log(`[${new Date()}] Completed English Premier League Logging`);
+  /**/
+
+  /**
+  // Get all German Bundesliga players.
+  console.log(`[${new Date()}] Running German Bundesliga Logging`);
+  await getESPNSoccer('ger.1', 'German Bundesliga');
+  console.log(`[${new Date()}] Completed German Bundesliga Logging`);
+  /**/
+
+  /**
+  // Get all Spanish LaLiga players.
+  console.log(`[${new Date()}] Running Spanish LaLiga Logging`);
+  await getESPNSoccer('esp.1', 'Spanish LaLiga');
+  console.log(`[${new Date()}] Completed Spanish LaLiga Logging`);
+  /**/
+
+  /**
+  // Get all Mexican Liga MX players.
+  console.log(`[${new Date()}] Running Mexican Liga MX Logging`);
+  await getESPNSoccer('mex.1', 'Mexican Liga MX');
+  console.log(`[${new Date()}] Completed Mexican Liga MX Logging`);
+  /**/
+
+  /** */
+  //Get all ATP players.
+  console.log(`[${new Date()}] Running ATP Logging`);
+  await getATP();
+  console.log(`[${new Date()}] Completed ATP Logging`);
+  /**/
+
+  /** */
+  //Get all WTA players.
+  console.log(`[${new Date()}] Running WTA Logging`);
+  await getWTA();
+  console.log(`[${new Date()}] Completed WTA Logging`);
+  /**/
+
+  console.log('---------------------');
+  console.log(`[${new Date()}] Completed startup...`);
+  console.log('---------------------');
+};
+
+startUp();
--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import getCFL from './external/cfl';
 import getBasketball, { getWNBA } from './external/basketball';
 import getMLS from './external/mls';
 import getESPNSoccer from './external/espnSoccer';
+import getATP, { getWTA } from './external/tennis';
 
 let hasStarted = false;
 const timeZone = 'America/Toronto';
@@ -22,9 +23,9 @@ process.on('SIGTERM', async () => {
   console.log('---------------------');
 });
 
-// Run at 11:00 every day.
+// Run at 9:00 every day.
 cron.schedule(
-  '00 11 * * *',
+  '00 9 * * *',
   async () => {
     if (!hasStarted) return;
 
@@ -40,9 +41,9 @@ cron.schedule(
   { timezone: timeZone }
 );
 
-// Run at 11:15 every day.
+// Run at 9:15 every day.
 cron.schedule(
-  '15 11 * * *',
+  '15 9 * * *',
   async () => {
     if (!hasStarted) return;
 
@@ -58,9 +59,9 @@ cron.schedule(
   { timezone: timeZone }
 );
 
-// Run at 11:30 every day.
+// Run at 9:30 every day.
 cron.schedule(
-  '30 11 * * *',
+  '30 9 * * *',
   async () => {
     if (!hasStarted) return;
 
@@ -76,9 +77,9 @@ cron.schedule(
   { timezone: timeZone }
 );
 
-// Run at 11:45 every day.
+// Run at 9:45 every day.
 cron.schedule(
-  '45 11 * * *',
+  '45 9 * * *',
   async () => {
     if (!hasStarted) return;
 
@@ -94,9 +95,9 @@ cron.schedule(
   { timezone: timeZone }
 );
 
-// Run at 12:00 every day.
+// Run at 10:00 every day.
 cron.schedule(
-  '00 13 * * *',
+  '00 10 * * *',
   async () => {
     if (!hasStarted) return;
 
@@ -112,9 +113,9 @@ cron.schedule(
   { timezone: timeZone }
 );
 
-// Run at 12:15 every day.
+// Run at 10:15 every day.
 cron.schedule(
-  '15 12 * * *',
+  '15 10 * * *',
   async () => {
     if (!hasStarted) return;
 
@@ -130,9 +131,9 @@ cron.schedule(
   { timezone: timeZone }
 );
 
-// Run at 12:30 every day.
+// Run at 10:30 every day.
 cron.schedule(
-  '30 12 * * *',
+  '30 10 * * *',
   async () => {
     if (!hasStarted) return;
 
@@ -148,9 +149,9 @@ cron.schedule(
   { timezone: timeZone }
 );
 
-// Run at 12:45 every day.
+// Run at 10:45 every day.
 cron.schedule(
-  '45 12 * * *',
+  '45 10 * * *',
   async () => {
     if (!hasStarted) return;
 
@@ -166,9 +167,9 @@ cron.schedule(
   { timezone: timeZone }
 );
 
-// Run at 13:00 every day.
+// Run at 11:00 every day.
 cron.schedule(
-  '00 13 * * *',
+  '00 11 * * *',
   async () => {
     if (!hasStarted) return;
 
@@ -188,9 +189,9 @@ cron.schedule(
   { timezone: timeZone }
 );
 
-// Run at 13:15 every day.
+// Run at 11:15 every day.
 cron.schedule(
-  '15 13 * * *',
+  '15 11 * * *',
   async () => {
     if (!hasStarted) return;
 
@@ -208,9 +209,9 @@ cron.schedule(
   { timezone: timeZone }
 );
 
-// Run at 13:30 every day.
+// Run at 11:30 every day.
 cron.schedule(
-  '30 13 * * *',
+  '30 11 * * *',
   async () => {
     if (!hasStarted) return;
 
@@ -226,88 +227,112 @@ cron.schedule(
   { timezone: timeZone }
 );
 
+// Run at 11:45 every day.
+cron.schedule(
+  '45 11 * * *',
+  async () => {
+    if (!hasStarted) return;
+
+    console.log('---------------------');
+    console.log(`[${new Date()}] Running ATP Logging`);
+
+    // Get all ATP players.
+    await getATP();
+
+    console.log(`[${new Date()}] Completed ATP Logging`);
+    console.log('---------------------');
+  },
+  { timezone: timeZone }
+);
+
+// Run at 12:00 every day.
+cron.schedule(
+  '00 12 * * *',
+  async () => {
+    if (!hasStarted) return;
+
+    console.log('---------------------');
+    console.log(`[${new Date()}] Running WTA Logging`);
+
+    // Get all WTA players.
+    await getWTA();
+
+    console.log(`[${new Date()}] Completed WTA Logging`);
+    console.log('---------------------');
+  },
+  { timezone: timeZone }
+);
+
 const startUp = async () => {
   hasStarted = false;
   console.log('---------------------');
   console.log(`[${new Date()}] Starting cron job scheduler...`);
   console.log('---------------------');
 
-  /** */
   // Get all NHL players.
   console.log(`[${new Date()}] Running NHL Logging`);
   await getHockey();
   console.log(`[${new Date()}] Completed NHL Logging`);
-  /**/
 
-  /** */
   // Get all MLB players.
   console.log(`[${new Date()}] Running MLB Logging`);
   await getBaseball();
   console.log(`[${new Date()}] Completed MLB Logging`);
-  /**/
 
-  /** */
   // Get all NFL players.
   console.log(`[${new Date()}] Running NFL Logging`);
   await getFootball();
   console.log(`[${new Date()}] Completed NFL Logging`);
-  /**/
 
-  /** */
   // Get all CFL players.
   console.log(`[${new Date()}] Running CFL Logging`);
   await getCFL();
   console.log(`[${new Date()}] Completed CFL Logging`);
-  /**/
 
-  /** */
   // Get all NBA players.
   console.log(`[${new Date()}] Running NBA Logging`);
   await getBasketball();
   console.log(`[${new Date()}] Completed NBA Logging`);
-  /**/
 
-  /** */
   // Get all WNBA players.
   console.log(`[${new Date()}] Running WNBA Logging`);
   await getWNBA();
   console.log(`[${new Date()}] Completed WNBA Logging`);
-  /**/
 
-  /** */
   // Get all MLS players.
   console.log(`[${new Date()}] Running MLS Logging`);
   await getMLS();
   console.log(`[${new Date()}] Completed MLS Logging`);
-  /**/
 
-  /** */
   // Get all Premier League players.
   console.log(`[${new Date()}] Running English Premier League Logging`);
   await getESPNSoccer('eng.1', 'English Premier League');
   console.log(`[${new Date()}] Completed English Premier League Logging`);
-  /**/
 
-  /** */
   // Get all German Bundesliga players.
   console.log(`[${new Date()}] Running German Bundesliga Logging`);
   await getESPNSoccer('ger.1', 'German Bundesliga');
   console.log(`[${new Date()}] Completed German Bundesliga Logging`);
-  /**/
 
-  /** */
   // Get all Spanish LaLiga players.
   console.log(`[${new Date()}] Running Spanish LaLiga Logging`);
   await getESPNSoccer('esp.1', 'Spanish LaLiga');
   console.log(`[${new Date()}] Completed Spanish LaLiga Logging`);
-  /**/
 
-  /** */
   // Get all Mexican Liga MX players.
   console.log(`[${new Date()}] Running Mexican Liga MX Logging`);
   await getESPNSoccer('mex.1', 'Mexican Liga MX');
   console.log(`[${new Date()}] Completed Mexican Liga MX Logging`);
-  /**/
+
+  //Get all ATP players.
+  console.log(`[${new Date()}] Running ATP Logging`);
+  await getATP();
+  console.log(`[${new Date()}] Completed ATP Logging`);
+
+  //Get all WTA players.
+  console.log(`[${new Date()}] Running WTA Logging`);
+  await getWTA();
+  console.log(`[${new Date()}] Completed WTA Logging`);
 
   console.log('---------------------');
   console.log(`[${new Date()}] Completed startup...`);
